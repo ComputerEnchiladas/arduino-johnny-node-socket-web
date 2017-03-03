b2cdbf8e4f71c7ec395089fa33fc9d340fa3ed75
JesusGuerrero <rocha.jesus@hotmail.com>
2017-03-03 04:39:33 -0800

    Server to Web and Arduino

--- server.js
+++ server.js
@@ -6,7 +6,33 @@ if (process.env.NODE_ENV === 'development') {
   console.log('Running Development!');
 }
 
+var io, five = require("johnny-five");
+var board = new five.Board();
+var state = false;
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+  led.off();
+  led.blink(5000, function(){
+    console.log( state );
+    state = !state;
+    io.sockets.emit('event:led:state', state);
+  });
+});
+
 require('mahrio')( process.env, __dirname ).then( function(server){
+
+  io = require('socket.io').listen( server.listener );
+
+  io.on('connection', function( socket ){
+    console.log('socket listening...' + socket.id); // Record the connection
+
+    socket.emit( 'event:hello' ); // Send message exclusively to new connection
+
+    socket.on( 'disconnect', function(){
+      console.log('goodbye socket...' + socket.id ); // Record the disconnection
+    });
+  });
 
   // ASSETS
   server.route({
